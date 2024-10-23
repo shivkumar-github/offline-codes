a64da25055fba6afd08eb09601b7385cb0e011cd
shivkumar <shivkumarrajmane164@gmail.com>
2024-10-23 20:53:29 +0530

    empty message

---
+++ chai aur code react playlist/03tailwindprops/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,html}',  // Ensures Tailwind scans your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
