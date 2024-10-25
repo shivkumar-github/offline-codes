cc2dfb581db955e6d4379c86f05a408ca936ccdf
shivkumar <shivkumarrajmane164@gmail.com>
2024-10-25 17:35:09 +0530

    empty message

---
+++ chai aur code react playlist/06currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-slate-500'>currency converter</h1>
+    </>
+  )
+}
+
+export default App
